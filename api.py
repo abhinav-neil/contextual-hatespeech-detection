b2a3ab32e4a2f1350b0c6e96451445c99f146fe9
Abhinav Bhuyan <54236926+abhinav-neil@users.noreply.github.com>
2023-06-17 16:41:44 +0000

    add lexicon & update api script

--- api.py
+++ api.py
@@ -1,99 +1,101 @@
-import requests
+import os
+import argparse
 from argparse import Namespace
-from getpass import getpass
-
-# Auto-generated documentation: 
+import requests
+from dotenv import load_dotenv
+# from getpass import getpass
 
 API = 'https://api.eooh.ai'
 PATHS = Namespace(
-    # Login
+    # login
     login       = '/request_token',
 
-    # Channels
+    # channels
     new_channel = '/channel/new',
     my_channels = '/channels/mine',
     get_channel = '/channel/{uid}',
 
-    # Lexicons
+    # lexicons
     add_lexicon    = '/lexicon/add',
     my_lexicons    = '/mylexicons',
     get_lexicon    = '/lexicon/{uid}',
-    delete_lexicon = '/lexicon/{uid}/delete', 
+    delete_lexicon = '/lexicon/{uid}/delete'
 )
 TOKEN = None
 
-def _req(method, endpoint, *args, **kwargs):
-    if endpoint != PATHS.login:
-        assert TOKEN, 'You must log in first.'
-        kwargs.setdefault('headers', {})
-        kwargs['headers']['Authorization'] = f'Bearer {TOKEN}'
-    
+def request_api(method, endpoint, *args, **kwargs):
+    '''Wrapper for requests.get and requests.post that adds the auth token.'''
+    headers = kwargs.setdefault('headers', {})
+    if TOKEN:
+        headers['Authorization'] = f'Bearer {TOKEN}'
+
     url = API+endpoint
-    return method(url, *args, **kwargs)
+    response = method(url, *args, **kwargs)
 
-get  = lambda *a, **kw: _req(requests.get,  *a, **kw)
-post = lambda *a, **kw: _req(requests.post, *a, **kw)
+    # check for expired token and refresh if necessary
+    if response.status_code == 401 and 'accessToken expired' in response.text:
+        print("Access token expired. Refreshing...")
+        new_token = login()
+        print(f"New token: {new_token}")
+        headers['Authorization'] = f'Bearer {new_token}'
+        return method(url, *args, **kwargs)  # Retry request with new token
 
-def login(email, password=None, digits=None, refresh=False):
-    """Returns an authentifying token, valid for 24 hours."""
-    if not TOKEN or refresh:
-        payload = {'username':email}
-        payload['password'] = password or getpass("Password: ") + (digits or input("2FA digits: "))
-        response =  post(PATHS.login, data=payload)
-        if not response:
-            raise ValueError(f'Login Failed: ({response.status_code}) - {response.text}')
-        
-        token = response.json()['accessToken']
+    return response
+
+def login():
+    """Returns an authenticating token, valid for 24 hours."""
+    username = os.getenv('EOOH_USERNAME')
+    pwd = os.getenv('EOOH_PASSWORD')
+    if not username or not pwd:
+        username = input("username: ")
+        pwd = input("password: ")
+    TFA_code = input("2FA code: ")
+    payload = {'username': username, 'password': pwd + TFA_code}
+    response = request_api(requests.post, PATHS.login, data=payload)
+    if not response:
+        raise ValueError(f'Login Failed: ({response.status_code}) - {response.text}')
+    token = response.json()['accessToken']
+
     return token
 
+def create_channel(lexicon_path):
+    '''Creates a new channel with the given lexicon.'''
+    # read lexicon file
+    with open(lexicon_path, 'r', encoding='utf-8') as file:
+        lexicon = file.read().splitlines()
 
-if __name__ == '__main__':
-    email = 'abhinav.bhuyan@student.uva.nl' 
-    TOKEN = login(email)
-    
-    # Create a new channel
-    payload = {
+    # create a new channel
+    channel_payload = {
         'name': 'test channel',
-        'public':False,
-        'search_parameters':{
-            'query':{
-                'en':['1488', 'oy vey', 'a future for white children']
+        'public': False,
+        'search_parameters': {
+            'query': {
+                'en': lexicon
             },
-            'platform':{
-                'Twitter':{} # No mandatory fields for Twitter
+            'platform': {
+                'Twitter': {},
+                'Facebook': {},
+                'Reddit': {
+                    'path': ['reddit.com/r/politics', 'reddit.com/r/darkjokes', 'reddit.com/r/meanjokes', 'reddit.com/r/offensivejokes']
+                }
             }
         }
     }
 
-    response = post(PATHS.new_channel, json=payload)
+    response = request_api(requests.post, PATHS.new_channel, json=channel_payload)
     channel = response.json()
-    print(channel)
+    print(f"New channel: {channel}")
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='EOOH API script.')
+    parser.add_argument('--lexicon-path', type=str, default='queer_lexicon.txt',
+                        help='Path to the lexicon file.')
+    args = parser.parse_args()
 
+    load_dotenv()
+    TOKEN = os.getenv('EOOH_API_KEY')
+    if not TOKEN:
+        TOKEN = login()
+        print(f"New token: {TOKEN}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
+    create_channel(args.lexicon_path)
